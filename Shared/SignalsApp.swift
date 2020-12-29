9c125f2b510b91d11e00b270f1ae7966cb613b42
Ben Strong <bstrong@gmail.com>
2020-12-28 19:17:40 -0600

    Initial Commit

---
+++ Shared/SignalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalsApp.swift
+//  Shared
+//
+//  Created by Ben Strong on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SignalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
